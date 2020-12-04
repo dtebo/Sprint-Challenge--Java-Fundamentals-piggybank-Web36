9fa37aeac2e657c79087ddd679eba066ff5a62f0
Darren Tebo <darrentebo83@outlook.com>
2020-12-04 11:27:36 -0500

    created inital java application

---
+++ javapiggybank/src/main/java/com/lambdaschool/javapiggybank/JavapiggybankApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.javapiggybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavapiggybankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavapiggybankApplication.class, args);
+    }
+
+}
